609b8bae712ad90162eaf177b70a34d78f86b0c3
Ryoung27 <RRYoung89@gmail.com>
2018-09-23 09:24:20 -0500

    simple tutorial

---
+++ Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var newString = "New variable."
+
+print(newString)
+
+var a = 2
+var b = 1
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+
