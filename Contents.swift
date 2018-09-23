22076ded9f51d5060a4e002f58d8bb26a92cebb4
Ryoung27 <RRYoung89@gmail.com>
2018-09-23 09:40:12 -0500

    After branching tutorial

--- Contents.swift
+++ Contents.swift
@@ -7,11 +7,19 @@ var newString = "New variable."
 print(newString)
 
 var a = 2
-var b = 1
+var b = -1
 
 print(a + b)
 print(a - b)
 print(a * b)
 print(a / b)
 
-
+if a < 4 && b < 4 {
+    print("Branch 1")
+}
+else if b < 1 {
+    print("Branch 2")
+}
+else {
+    print("Branch 3")
+}
